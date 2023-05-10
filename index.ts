682dd8ef31e690bd6ba25bde0d6cf95ef330e600
PrexCodes <meandmysystems@gmail.com>
2023-05-10 01:42:37 +0100

    second version

--- index.ts
+++ index.ts
@@ -19,13 +19,45 @@ class Stack{
     }
 }
 
-let ourStack = new Stack(); //--Instance of a class
+// let ourStack = new Stack(); //--Instance of a class
 
-ourStack.add("Okus")
-ourStack.add("Precious")
-ourStack.add("Solomon")
-ourStack.add("Faith")
-ourStack.add("Fash")
-ourStack.remove()
-ourStack.remove()
-ourStack.showArray()
+// ourStack.add("Okus")
+// ourStack.add("Precious")
+// ourStack.add("Solomon")
+// ourStack.add("Faith")
+// ourStack.add("Fash")
+// ourStack.remove()
+// ourStack.remove()
+// ourStack.showArray()
+
+
+//for queues - firstin firstout principle
+class Queues{
+
+    queue: Array<any>
+    constructor(){
+        this.queue = []
+    }
+
+    add(item){
+        this.queue.push(item)
+    }
+    remove(){
+        this.queue.shift()
+    }
+    showArray(){
+        console.log(this.queue)
+    }
+}
+
+let queue = new Queues()
+queue.add('Faith')
+queue.add('Spirit')
+queue.add('Oil')
+queue.add('Groceries')
+queue.add('Don')
+queue.add('Faith')
+queue.remove()
+queue.remove()
+queue.remove()
+queue.showArray()
